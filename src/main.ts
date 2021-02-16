501ed6853d7be4e1d3f0da103e0eda46d3a912bc
Richard Macklin <richard.github@nrm.com>
2021-02-15 21:38:17 -0800

    Implement basic functionality to add (dummy) labels to pull requests
    For now, the action's labeling logic is very silly: it adds an `even`
    label or an `odd` label according to the PR number. We'll ultimately
    replace this with the real logic, but this is a sufficient proof of
    concept to demonstrate that the process of adding labels is working.
    
    The `getPrNumber` and `addLabels` functions were copied from
    https://github.com/actions/labeler, an action created by GitHub for
    labeling pull requests based on their changed files.

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,55 @@
 import * as core from '@actions/core';
-import { wait } from './wait';
+import * as github from '@actions/github';
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds');
-    core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const token = core.getInput('repo_token', { required: true });
 
-    core.debug(new Date().toTimeString());
-    await wait(parseInt(ms, 10));
-    core.debug(new Date().toTimeString());
+    const prNumber = getPrNumber();
+    if (!prNumber) {
+      throw new Error('Could not get pull request number from context');
+    }
 
-    core.setOutput('time', new Date().toTimeString());
+    const client = new github.GitHub(token);
+    const labels = [];
+
+    if (prNumber % 2 === 0) {
+      labels.push('even');
+    } else {
+      labels.push('odd');
+    }
+
+    core.debug(`labels to add: ${JSON.stringify(labels)}`);
+
+    if (labels.length > 0) {
+      await addLabels(client, prNumber, labels);
+    }
   } catch (error) {
+    core.error(error);
     core.setFailed(error.message);
   }
+}
+
+function getPrNumber(): number | undefined {
+  const pullRequest = github.context.payload.pull_request;
+  if (!pullRequest) {
+    return undefined;
+  }
+
+  return pullRequest.number;
+}
+
+async function addLabels(
+  client: github.GitHub,
+  prNumber: number,
+  labels: string[]
+): Promise<void> {
+  await client.issues.addLabels({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    issue_number: prNumber,
+    labels
+  });
 }
 
 run();
